e53a4775242fe71c653e0d6af552468450ea321a
Martynas Binisevičius <126594886+MartinBin@users.noreply.github.com>
2024-09-27 09:37:07 +0300

    trying out client creation

---
+++ Mafia/Mafia-client/Join.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Mafia_client;
+
+public partial class Join : Page
+{
+    public Join()
+    {
+        InitializeComponent();
+    }
+}
--- Mafia/Mafia-server/obj/Debug/net8.0/Mafia-server.AssemblyInfo.cs
+++ Mafia/Mafia-server/obj/Debug/net8.0/Mafia-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mafia-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8a51284934cf65731fc9c44b8f346a50b366ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mafia-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mafia-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
