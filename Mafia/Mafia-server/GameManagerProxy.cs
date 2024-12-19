381be7f48b8d50e2416d504e225bcdf4ca3aaaf1
weirdhumanbeing <gustas.miliukas@gmail.com>
2024-12-19 17:37:37 +0200

    [UPD] Proxy

--- Mafia/Mafia-server/GameManagerProxy.cs
+++ Mafia/Mafia-server/GameManagerProxy.cs
@@ -3,14 +3,35 @@
     public class GameManagerProxy : IGameManager
     {
         private IGameManager _gameManager;
+        private bool isAccessible;
         public GameManagerProxy(IGameManager gameManager)
         {
             _gameManager = gameManager;
+            isAccessible = true;
+        }
+
+        public void GiveAccess()
+        {
+            isAccessible = true;
+        }
+
+        public void RemoveAccess()
+        {
+            isAccessible = false;
+        }
+
+        private bool CheckAccess()
+        {
+            return isAccessible;
         }
 
         public void AssignCharactersToPlayers(List<Player> players)
         {
-            _gameManager.AssignCharactersToPlayers(players);
+            if (CheckAccess())
+            {
+                _gameManager.AssignCharactersToPlayers(players);
+            }
+            
         }
     }
 }
